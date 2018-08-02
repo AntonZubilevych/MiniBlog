4e800acf8329db233f97d47012bf34afd439ea33
AntonZubb <antonzubb@gmail.com>
2018-08-02 12:39:15 +0300

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -30,29 +30,4 @@ class HomeController extends AbstractController
             'page' => $page
         ]);
     }
-
-    public function new()
-    {
-        $manager = $this->getDoctrine()->getManager();
-
-        $model = new Page();
-        $model->setPageName('Php7');
-        $model->setTitle('PHP (англ. PHP: Hypertext Preprocessor — PHP:гіпертекстовий препроцесор');
-        $model->setAboutContent('PHP (англ. PHP: Hypertext Preprocessor — PHP:
-        гіпертекстовий препроцесор), попередня назва: Personal Home Page Tools — скриптова мова програмування,
-        була створена для генерації HTML-сторінок на стороні веб-сервера. PHP є однією з найпоширеніших мов,
-        що використовуються у сфері веб-розробок (разом із Java, .NET, Perl, Python, Ruby). PHP підтримується
-        переважною більшістю хостинг-провайдерів. 
-        PHP — проект відкритого програмного забезпечення..');
-        $model->setAdditionalInfo(' (разом із Java, .NET, Perl, Python, Ruby). PHP підтримується
-        переважною більшістю хостинг-провайдерів. 
-        PHP — проект відкритого програмного забезпечення..\')');
-        $model->setShowAdditionalInfo(false);
-        $model->setUpdatedAt(new \DateTime());
-
-        $manager->persist($model);
-        $manager->flush();
-
-        return new Response('Saved new product with id '.$model->getId());
-    }
 }
